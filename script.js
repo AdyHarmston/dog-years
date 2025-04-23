215b9e18234030ce8d0304282d0d2b95d9140cbc
AdyHarmston <adyson.harmston@gmail.com>
2025-04-22 20:58:53 -0600

    Initial commit

--- script.js
+++ script.js
@@ -5,20 +5,21 @@ document.getElementById("btnCalc").onclick = function(){
    let humanAge = 0;
    let dogSize = document.getElementById("radLarge")
    if(dogAge >= 1){
-    humanAge += 15;
+     humanAge += 15;
+
     }
     if(dogAge >= 2){
-    humanAge +=9;
+        humanAge += 9;
     }
     if(dogAge >= 3){
-    let years = dogAge -2;
-    humanAge += years * 4.5;
+        if(dogSize.checked);{
+        humanAge += (dogAge-2) * 5;
     }
     else{
             humaneAge += (dogAge-2) * 5:
     }
     else{
-        humanAge += ("dogAge-2") * 4;
+        humanAge += (dogAge-2) * 4;
         }
 
     }
