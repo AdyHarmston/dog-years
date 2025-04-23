1f3403c59ad6ae2348abbdba1ccbdb722a929ada
AdyHarmston <adyson.harmston@gmail.com>
2025-04-22 20:34:48 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+document.getElementById("btnCalc").onclick = function(){
+   let dogAge = document.getElementById("txtDogAge").value;
+   let humanAge = 0;
+   let dogSize = document.getElementById("radLarge")
+   if(dogAge >= 1){
+    humanAge += 15;
+    }
+    if(dogAge >= 2){
+    humanAge +=9;
+    }
+    if(dogAge >= 3){
+    let years = dogAge -2;
+    humanAge += years * 4.5;
+    }
+    else{
+            humaneAge += (dogAge-2) * 5:
+    }
+    else{
+        humanAge += ("dogAge-2") * 4;
+        }
+
+    }
+    document.getElementById("result").innerHTML = "Your dog is " + humanAge;
+}
